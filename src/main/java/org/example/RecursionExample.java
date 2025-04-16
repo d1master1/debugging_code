5c7dcfdd8ae12610afd0bd06ee3f8264e7b061e5
d1_master1 <barancev2006@gmail.com>
2025-04-16 09:54:32 +0300

    public class RecursionExample {

--- src/main/java/org/example/RecursionExample.java
+++ src/main/java/org/example/RecursionExample.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class RecursionExample {
     public static void infiniteRecursion(int n) {
+        if (n > 1000) return; // Остановка после 1000 вызовов
         System.out.println("n = " + n);
-        infiniteRecursion(n + 1); // Ошибка! Нет условия выхода
+        infiniteRecursion(n + 1);
     }
 
     public static void main(String[] args) {
