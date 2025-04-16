ca94a2cbcc5f433dd6cc94ebdf7cad340d27cdd9
d1_master1 <barancev2006@gmail.com>
2025-04-16 10:15:35 +0300

    Correct multiolication function

--- src/main/java/org/example/RecursionExample.java
+++ src/main/java/org/example/RecursionExample.java
@@ -1,31 +1,7 @@
 package org.example;
 
-class DeadlockFixed {
-    private static final Object lock1 = new Object();
-    private static final Object lock2 = new Object();
-
-    public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
-            synchronized (lock1) {
-                System.out.println("Thread 1: Holding lock1...");
-                try { Thread.sleep(100); } catch (InterruptedException e) {}
-                synchronized (lock2) {
-                    System.out.println("Thread 1: Holding lock2...");
-                }
-            }
-        });
-
-        Thread t2 = new Thread(() -> {
-            synchronized (lock1) { // Изменён порядок блокировки
-                System.out.println("Thread 2: Holding lock1...");
-                try { Thread.sleep(100); } catch (InterruptedException e) {}
-                synchronized (lock2) {
-                    System.out.println("Thread 2: Holding lock2...");
-                }
-            }
-        });
-
-        t1.start();
-        t2.start();
+public class RecursionExample {
+    public static int multiply(int a, int b) {
+        return a * b; // Корректная версия
     }
 }
